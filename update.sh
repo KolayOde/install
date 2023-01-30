24107cbe997ead68b5ac46ad4e4c0c21357243fe
oguzhankocatepe <oguzhankocatepe@gmail.com>
2023-01-30 17:40:53 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ mv buildagent/appsettings.js .
 rm -r buildagent
 mv deployment/buildagent .
 mv appsettings.js buildagent
-mv deployment/Debug/netcoreapp3.1/3cx-WebApi.dll Debug/netcoreapi3.1
+mv deployment/Debug/netcoreapp3.1/3cx-WebApi.dll Debug/netcoreapp3.1
 rm -r deployment
 systemctl restart nginx
 pidof dotnet | xargs kill -9
