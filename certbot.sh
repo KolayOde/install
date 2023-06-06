c7ecef8dcef8da2407ac41db01821458f74fafac
oguzhankocatepe <oguzhankocatepe@gmail.com>
2023-06-07 02:32:11 +0300

    Create certbot.sh

---
+++ certbot.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+cd /home
+apt-get install certbot
+wget https://github.com/joohoi/acme-dns-certbot-joohoi/raw/master/acme-dns-auth.py
+chmod +x acme-dns-auth.py
+nano acme-dns-auth.py
+mv acme-dns-auth.py /etc/letsencrypt/
+certbot certonly --standalone
+openssl pkcs12 -inkey privkey.pem -in fullchain.pem -export -out rootCA.pfx
