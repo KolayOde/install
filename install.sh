4f12f1e1f988b788283ba0b4ade3ffed2f4825f9
oguzhankocatepe <oguzhankocatepe@gmail.com>
2023-06-08 10:26:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,14 +12,14 @@ rm -r deployment
 sed -i 's/IP/'"`hostname -fqdn`"'/g' build/appsettings.js
 sed -i 's/IP/'"`hostname -fqdn`"'/g' buildagent/appsettings.js
 apt-get install -y curl
-cat install/root >> /var/spool/cron/crontabs/root
+cat release/root >> /var/spool/cron/crontabs/root
 service cron reload
 systemctl restart cron
-mv install/kestrel-netcoreapp.service /etc/systemd/system
+mv release/kestrel-netcoreapp.service /etc/systemd/system
 systemctl enable /etc/systemd/system/kestrel-netcoreapp.service
 systemctl start kestrel-netcoreapp.service
-cat install/mail.txt >> /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini
+cat release/mail>> /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini
 chmod -w /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini
-cat install/nginx.conf >> /var/lib/3cxpbx/Bin/nginx/conf/nginx.conf
+cat release/nginx >> /var/lib/3cxpbx/Bin/nginx/conf/nginx.conf
 chmod -w /var/lib/3cxpbx/Bin/nginx/conf/nginx.conf
 systemctl restart nginx
