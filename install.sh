1c7a9c473fc93005bf6c7ea39958657b8d62a5b0
oguzhankocatepe <oguzhankocatepe@gmail.com>
2023-12-17 23:18:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,22 @@ apt-get install -y git
 git clone https://github.com/KolayOde/deployment.git
 mv deployment/* .
 rm -r deployment
-sed -i 's/IP/'"`hostname -f`"'/g' build/appsettings.js
-sed -i 's/IP/'"`hostname -f`"'/g' buildagent/appsettings.js
+sed -i 's/IP/'"$1"'/g' build/appsettings.js
+sed -i 's/IP/'"$1"'/g' buildagent/appsettings.js
+sed -i 's/*/'"$1"'/g' netcoreapp3.1/appsettings.json
 apt-get install -y curl
 cat release/root >> /var/spool/cron/crontabs/root
-sed -i 's/localhost/'"`hostname -f`"'/g' /var/spool/cron/crontabs/root
+sed -i 's/localhost/'"$1"'/g' /var/spool/cron/crontabs/root
 service cron reload
 systemctl restart cron
+openssl pkcs12 -inkey "$1"-key.pem -in "$1"-crt.pem -export -out "$1"CA.pfx
+cp /var/lib/3cxpbx/Bin/nginx/conf/Instance1/*.pfx /root
 mv release/kestrel-netcoreapp.service /etc/systemd/system
 systemctl enable /etc/systemd/system/kestrel-netcoreapp.service
 systemctl start kestrel-netcoreapp.service
 cat release/mail>> /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini
 chmod -w /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini
 cat release/nginx >> /var/lib/3cxpbx/Bin/nginx/conf/nginx.conf
-sed -i 's/FQDN/'"`hostname -f`"'/g' /var/lib/3cxpbx/Bin/nginx/conf/nginx.conf
+sed -i 's/FQDN/'"$1"'/g' /var/lib/3cxpbx/Bin/nginx/conf/nginx.conf
 chmod -w /var/lib/3cxpbx/Bin/nginx/conf/nginx.conf
 systemctl restart nginx
