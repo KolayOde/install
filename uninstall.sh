21cf5f38f2c2ee32c27fb0dad53eee125a802c5c
oguzhankocatepe <oguzhankocatepe@gmail.com>
2024-12-12 01:10:53 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,7 @@ apt-get remove dotnet-sdk-3.1
 dpkg --purge packages-microsoft-prod
 apt-get autoremove
 nano /etc/apt/sources.list
+rm /etc/apt/sources.list.d/microsoft-prod.list
 apt-get update
 ps -ef | grep dotnet
 kill -9 13213
@@ -13,6 +14,7 @@ service cron reload
 rm /etc/systemd/system/kestrel-netcoreapp.service
 rm /etc/systemd/system/multi-user.target.wants/kestrel-netcoreapp.service
 systemctl daemon-reload
+systemctl reset-failed
 rm /root/*.pfx
 rm /var/lib/3cxpbx/Bin/nginx/conf/Instance1/*.pfx
 nano /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini
